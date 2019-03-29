6a6095affc6df2a52feda5bac3c720bb0dcad498
Thyago Stall <thstall@gmail.com>
2019-03-29 19:33:14 -0300

    Improvements on Movie index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,25 +8,27 @@
                     <div class="card-header">{{ __('Movies') }}</div>
 
                     <div class="card-body">
+                        <a class="btn btn-link" href="{{ route('movies.create') }}">{{ __('New movie') }}</a>
                         <table class="table table-hover">
                             <thead>
                             <tr>
                                 <th scope="col">{{ __('Name')  }}</th>
                                 <th scope="col">{{ __('Original name')  }}</th>
                                 <th scope="col">{{ __('Duration')  }}</th>
-                                <th scope="col">{{ __('Slug')  }}</th>
+                                <th scope="col" colspan="2">{{ __('Slug')  }}</th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($movies as $movie)
                                 <tr>
-                                    <td>{{ $movie->name }}</td>
+                                    <td><a href="{{ route('movies.show', [$movie->slug]) }}">{{ $movie->name }}</a></td>
                                     <td>{{ $movie->original_name }}</td>
-                                    <td>{{ $movie->duration_in_minutes }} minutes</td>
-                                    <td><a href="{{ route('movies.edit', [$movie->slug]) }}">{{ $movie->slug }}</a></td>
+                                    <td>{{ $movie->duration_in_minutes . __(' minutes') }}</td>
+                                    <td>{{ $movie->slug }}</td>
+                                    <td><a href="{{ route('movies.edit', [$movie->slug]) }}">Edit</a></td>
                                 </tr>
-                            </tbody>
                             @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
