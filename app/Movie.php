e236213c363915fa72155737d9cab90c7fe6c456
Thyago Stall <thstall@gmail.com>
2019-03-30 23:35:49 -0300

    Declare Movie fields

--- app/Movie.php
+++ app/Movie.php
@@ -4,6 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string slug
+ * @property string name
+ * @property string original_name
+ * @property int duration_in_minutes
+ * @property string plot_summary
+ * @property string image_url
+ */
 class Movie extends Model
 {
     protected $primaryKey = 'slug';
@@ -16,6 +24,5 @@ class Movie extends Model
         'duration_in_minutes',
         'plot_summary',
         'image_url',
-        'plot_summary',
     ];
 }
